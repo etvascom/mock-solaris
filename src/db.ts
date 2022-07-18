195145a851ce8eebb14fd6f7a7e88c753bc432c6
Calin Bogdan <calin@etvas.com>
2022-07-18 17:18:47 +0300

    refactor seeding

--- src/db.ts
+++ src/db.ts
@@ -3,8 +3,6 @@ import Promise from "bluebird";
 
 import * as log from "./logger";
 import { calculateOverdraftInterest } from "./helpers/overdraft";
-import { seedTransactions } from "./seeds/transactions";
-import { seedAccount } from "./seeds/accounts";
 import { seedPersons } from "./seeds/persons";
 
 let redis;
@@ -28,102 +26,7 @@ redisClient.on("error", (err) => {
 });
 
 export const migrate = async () => {
-  try {
-    await getPerson("demo100");
-    throw new Error("during development, we create it every time");
-  } catch (error) {
-    log.warn("kontistGmbHAccount not found, creating");
-
-    const accounts = [
-      { ...seedAccount("demo100"), type: "CHECKING_BUSINESS" },
-      {
-        ...seedAccount("demo100"),
-        locking_status: "BLOCK",
-        locking_reasons: ["COMPLIANCE"],
-      },
-      {
-        ...seedAccount("demo100"),
-        locking_status: "DEBIT_BLOCK",
-        locking_reasons: ["COMPLIANCE"],
-      },
-      {
-        ...seedAccount("demo100"),
-        locking_status: "CREDIT_BLOCK",
-        locking_reasons: ["COMPLIANCE"],
-      },
-      { ...seedAccount("demo100") },
-    ];
-
-    const person = {
-      salutation: "MR",
-      first_name: "Kontist",
-      last_name: "GmbH",
-      birth_date: "1998-01-01T00:00:00.000Z",
-      birth_city: "Copenhagen",
-      nationality: "DE",
-      employment_status: "FREELANCER",
-      birth_country: "DE",
-      address: {
-        line_1: "Torstraße 177",
-        postal_code: "10155",
-        city: "Berlin",
-        country: "DE",
-      },
-      fatca_relevant: true,
-      email: "kontistgmbh@mocksolaris.example.com",
-      mobile_number: "+49123123223",
-      id: "demo100",
-      identifications: {
-        "identify-mock691f4e49fc43b913bd8ede668e187e9a-1509032370615": {
-          id: "identify-mock691f4e49fc43b913bd8ede668e187e9a-1509032370615",
-          reference: null,
-          url:
-            "https://go.test.idnow.de/kontist/identifications/identify-mock691f4e49fc43b913bd8ede668e187e9a-1509032370615",
-          status: "successful",
-          completed_at: null,
-          method: "idnow",
-          identificationLinkCreatedAt: "2017-10-26T15:39:31.327Z",
-          person_id: "mock691f4e49fc43b913bd8ede668e187e9a",
-          startUrl:
-            "https://api.test.idnow.de/api/v1/kontist/identifications/identify-mock691f4e49fc43b913bd8ede668e187e9a-1509032370615/start",
-          email: "i1@kontist.com",
-        },
-        "identify-mock691f4e49fc43b913bd8ede668e187e9a-1509032371343": {
-          id: "identify-mock691f4e49fc43b913bd8ede668e187e9a-1509032371343",
-          reference: null,
-          url: null,
-          status: "created",
-          completed_at: null,
-          method: "idnow",
-        },
-      },
-      screening_progress: null,
-      transactions: _.flatten(
-        accounts.map(({ id }) => seedTransactions(5, id))
-      ),
-      account: accounts[0],
-      accounts,
-      billing_account: {
-        id: process.env.SOLARIS_KONTIST_BILLING_ACCOUNT_ID,
-        iban: "DE58110101002263909949",
-        bic: process.env.SOLARIS_BIC,
-        type: "CHECKING_BUSINESS",
-        person_id: "demo100",
-        balance: {
-          value: 100,
-        },
-        sender_name: "unknown",
-        locking_status: "",
-        available_balance: {
-          value: 100,
-        },
-      },
-    };
-
-    await savePerson(person);
-
-    await seedPersons(person, savePerson);
-  }
+  await seedPersons(savePerson);
 };
 
 const jsonToPerson = (value) => {
--- src/seeds/persons.ts
+++ src/seeds/persons.ts
@@ -1,29 +1,107 @@
 import { seedAccount } from "./accounts";
 import { seedTransactions } from "./transactions";
+import _ from "lodash";
 
-const PERSON_COUNT = parseInt(process.env.PERSON_COUNT, 10) || 100;
+const PERSON_COUNT = parseInt(process.env.PERSON_COUNT, 10) || 1000;
 
-export const seedPersons = async (basePerson, savePerson: (person) => {}) => {
-  return Promise.all(
-    Array.from({ length: PERSON_COUNT }).map((__, index) => {
-      const formattedIndex = ("0" + index).slice(-2);
+const createNewPerson = (personId: string) => {
+  const accounts = [
+    { ...seedAccount(personId), type: "CHECKING_BUSINESS" },
+    {
+      ...seedAccount(personId),
+      locking_status: "BLOCK",
+      locking_reasons: ["COMPLIANCE"],
+    },
+    {
+      ...seedAccount(personId),
+      locking_status: "DEBIT_BLOCK",
+      locking_reasons: ["COMPLIANCE"],
+    },
+    {
+      ...seedAccount(personId),
+      locking_status: "CREDIT_BLOCK",
+      locking_reasons: ["COMPLIANCE"],
+    },
+    { ...seedAccount(personId) },
+  ];
 
-      const id = `demo${formattedIndex}`;
-      const email = `demo+${formattedIndex}@${process.env.EMAIL_DOMAIN}`;
-      const firstName = id;
+  return {
+    salutation: "MR",
+    first_name: personId,
+    last_name: "GmbH",
+    birth_date: "1998-01-01T00:00:00.000Z",
+    birth_city: "Copenhagen",
+    nationality: "DE",
+    employment_status: "FREELANCER",
+    birth_country: "DE",
+    address: {
+      line_1: "Torstraße 177",
+      postal_code: "10155",
+      city: "Berlin",
+      country: "DE",
+    },
+    fatca_relevant: true,
+    email: `${personId}@email.com`,
+    mobile_number: "+49123123223",
+    id: personId,
+    identifications: {
+      "identify-mock691f4e49fc43b913bd8ede668e187e9a-1509032370615": {
+        id: "identify-mock691f4e49fc43b913bd8ede668e187e9a-1509032370615",
+        reference: null,
+        url:
+          "https://go.test.idnow.de/kontist/identifications/identify-mock691f4e49fc43b913bd8ede668e187e9a-1509032370615",
+        status: "successful",
+        completed_at: null,
+        method: "idnow",
+        identificationLinkCreatedAt: "2017-10-26T15:39:31.327Z",
+        person_id: "mock691f4e49fc43b913bd8ede668e187e9a",
+        startUrl:
+          "https://api.test.idnow.de/api/v1/kontist/identifications/identify-mock691f4e49fc43b913bd8ede668e187e9a-1509032370615/start",
+        email: "i1@kontist.com",
+      },
+      "identify-mock691f4e49fc43b913bd8ede668e187e9a-1509032371343": {
+        id: "identify-mock691f4e49fc43b913bd8ede668e187e9a-1509032371343",
+        reference: null,
+        url: null,
+        status: "created",
+        completed_at: null,
+        method: "idnow",
+      },
+    },
+    screening_progress: null,
+    transactions: _.flatten(accounts.map(({ id }) => seedTransactions(5, id))),
+    account: accounts[0],
+    accounts,
+    billing_account: {
+      id: process.env.SOLARIS_KONTIST_BILLING_ACCOUNT_ID,
+      iban: "DE58110101002263909949",
+      bic: process.env.SOLARIS_BIC,
+      type: "CHECKING_BUSINESS",
+      person_id: personId,
+      balance: {
+        value: 100,
+      },
+      sender_name: "unknown",
+      locking_status: "",
+      available_balance: {
+        value: 100,
+      },
+    },
+  };
+};
 
-      const acc = seedAccount(id);
+type PersistPersonFunc = (person: any) => Promise<void>;
 
-      return savePerson({
-        ...basePerson,
-        id,
-        email,
-        first_name: firstName,
-        last_name: "Demo",
-        transactions: seedTransactions(1, acc.id),
-        accounts: [acc],
-        account: acc,
-      });
-    })
-  );
+const DIGITS_FOR_ID = PERSON_COUNT.toString().length;
+
+export const seedPersons = async (savePerson: PersistPersonFunc) => {
+  _.range(0, PERSON_COUNT)
+    .map((number) =>
+      `demo`.concat(number.toString().padStart(DIGITS_FOR_ID, "0"))
+    )
+    .forEach((id) =>
+      savePerson({
+        ...createNewPerson(id),
+      })
+    );
 };
