499f8e8bdf6ba613a45d9e63e3cad7dd280c05e5
Tudor Hutu <hutu.tudor@yahoo.com>
2023-04-10 11:43:26 +0300

    chore: remove unused webhook secret hashing

--- src/helpers/webhooks.ts
+++ src/helpers/webhooks.ts
@@ -75,23 +75,15 @@ export const triggerWebhook = async (type, payload, extraHeaders = {}) => {
     ...payload,
   };
 
-  if (WEBHOOK_SECRETS[type]) {
-    const solarisWebhookSignature = generateSolarisWebhookSignature(
-      body,
-      WEBHOOK_SECRETS[type]
-    );
-
-    headers = {
-      ...headers,
-      "solaris-entity-id": body.id,
-      "solaris-webhook-attempt": "1",
-      "solaris-webhook-event-type": type,
-      "solaris-webhook-id": uuid.v4(),
-      "solaris-webhook-signature": solarisWebhookSignature,
-      "solaris-webhook-subscription-id": "STATIC-SUBSCRIPTION",
-      ...extraHeaders,
-    };
-  }
+  headers = {
+    ...headers,
+    "solaris-entity-id": body.id,
+    "solaris-webhook-attempt": "1",
+    "solaris-webhook-event-type": type,
+    "solaris-webhook-id": uuid.v4(),
+    "solaris-webhook-subscription-id": "STATIC-SUBSCRIPTION",
+    ...extraHeaders,
+  };
 
   await fetch(webhook.url, {
     method: "POST",
